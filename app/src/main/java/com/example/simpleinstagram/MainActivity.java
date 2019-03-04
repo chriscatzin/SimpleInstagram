84827e029861127e64c6f0e1b6d5d8a5c7ac7dc2
Chris Catzin <ckc196@humboldt.edu>
2019-03-03 22:11:32 -0800

    Initial commit

---
+++ app/src/main/java/com/example/simpleinstagram/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.simpleinstagram;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+    
+    private static final String TAG = "MainActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        //final EditText usernameInput = findViewById(R.id.usernameInput);
+        //final EditText passwordInput = findViewById(R.id.passwordInput);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+                Log.d("MainActivity","login successful");
+            }
+        });
+    }
+    private void goMainActivity() {
+        Log.d(TAG, "Navigating to App Activity");
+        Intent i = new Intent(this, AppActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
